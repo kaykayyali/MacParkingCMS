8cb5f5f38efe094d9eb2fcd947bfac40c710e481
Kaysser Kayyali <kaykayyali@gmail.com>
2015-09-12 16:02:42 -0400

    Added new functionality to the Notes Page and Added Note Creation page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,20 @@
 class NotesController < ApplicationController
 	before_action(:authenticate_user!)
 	def index
+		@notes = Note.all.where('user_id = ?', current_user.id)
 		render 'index'
 	end
 	def create
-		newNote = Note.new()
-
+		newNote = Note.new(note_params)
+		newNote.user_id = current_user.id
+		if newNote.save 
+			redirect_to('/notes')
+		end
 	end
 	def new
 		render 'new'
+	end
+	def note_params
+			params.permit(:content, :date, :subject)
 	end
 end
