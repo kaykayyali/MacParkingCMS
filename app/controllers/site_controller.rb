d6dbb33779841422cf6a80145aa9d42899ada380
Kaysser Kayyali <kaykayyali@gmail.com>
2015-09-12 16:07:05 -0400

    When user is signed in, redirect to /notes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,5 @@
 class SiteController < ApplicationController
-	
+	before_action(:check_for_user)
 	def index
 		render 'index'
 	end
@@ -13,5 +13,9 @@ class SiteController < ApplicationController
 		ContactMessage.create(name: params[:name], email: params[:email], phone: params[:phone],content: params[:content])
 		render status: 200, json: {response: "Worked"}
 	end
-	
+	def check_for_user
+		if current_user.present?
+			redirect_to("/notes")
+		end
+	end
 end
