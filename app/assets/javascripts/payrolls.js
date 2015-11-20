5b574985a3a4efeac59ce12ec55731dedf5f527a
Kaysser Emad Kayyali <kaysserk.admin@LMRC6494.local>
2015-11-20 17:37:22 -0500

    New way of handling payroll data. Should be done tonight.

--- app/assets/javascripts/payrolls.js
+++ app/assets/javascripts/payrolls.js
@@ -19,46 +19,40 @@ $(function() {
 	     }
 	});
 });
-var entries_count = 0
+var payroll_data = [];
+var entries_count = 0;
 var handle_pay_input_submission = function() {
-		var name = $('#employee_name').val(),
+		var employee_name = $('#employee_name').val(),
 	    	gross_pay = $('#pay_input').val()
 	    if (name != undefined && gross_pay != "" && gross_pay > 0) {
 	    	entries_count = entries_count + 1
 		    var  medicare_tax = (gross_pay * .0145),
 		    	employers_tax = (gross_pay * .062),
 		    	net_pay = roundToTwo((gross_pay - medicare_tax - employers_tax)),
-		    	new_item = "<li class='payroll_item clearfix' id ='"+(entries_count)+"'> <div class='col-xs-3 payroll_item_" + entries_count + "_name'><b>" + name + "</b></div><div class='col-xs-9 text-right payroll_item_" + entries_count + "_info'><span class='gross_pay'>Gross Pay: $" +gross_pay+ "</span> Employers Tax: <b>$" + roundToTwo(employers_tax) + "</b> Medicare Tax: <b>$" + roundToTwo(medicare_tax) + "</b> Net Pay: <b>$" + net_pay + "</b></div></li>"
-			$('#payroll_items').prepend(new_item)
+		    	new_payroll_data = {
+		    		name 	  :  employee_name,
+		    		gross     :  gross_pay,
+		    		medicare  : (gross_pay * .0145),
+		    		employers : (gross_pay * .062),
+		    		net 	  :  net_pay
+		    	},
+		    	new_item = "<li class='payroll_item clearfix' id ='"+(entries_count)+"'> <div class='col-xs-3 payroll_item_" + entries_count + "_name'><b>" + employee_name + "</b></div><div class='col-xs-9 text-right payroll_item_" + entries_count + "_info'><span class='gross_pay'>Gross Pay: $" +gross_pay+ "</span> Employers Tax: <b>$" + roundToTwo(employers_tax) + "</b> Medicare Tax: <b>$" + roundToTwo(medicare_tax) + "</b> Net Pay: <b>$" + net_pay + "</b></div></li>";
+			$('#payroll_items').prepend(new_item);
+			payroll_data.push(new_payroll_data);
 		} 
 		else if (name == undefined || name == "" ) {
-			alertBox("Name is empty or invalid")
+			alertBox("Name is empty or invalid");
 		}
 		else if (gross_pay == undefined || gross_pay <= 0 ) {
-			alertBox("Pay is empty or invalid")
+			alertBox("Pay is empty or invalid");
 		} 
 		else {
-			alertBox("Unidentified error.")
+			alertBox("Unidentified error.");
 		}
 }
-
 var handle_payroll_save = function() {
-	var payroll_items  = $('.payroll_item'),
-	payroll_data = collect_payroll_data(payroll_items);
+	var payroll_items  = $('.payroll_item')
 	console.log(payroll_data);
-}
-
-var collect_payroll_data = function(items) {
-	var data = []
-	$.each(items, function(index, item) {
-		var name = $('.payroll_item_' + (index + 1) + '_name').text(),
-		info_unsanitized = $('.payroll_item_' + (index + 1) + '_info').text();
-		data.push({
-			employee_name: name, 
-			employee_info: info_unsanitized
-		})
-	})
-	return data;
 }
 var roundToTwo = function(value) {
 	return(Math.round(value * 100) / 100);
