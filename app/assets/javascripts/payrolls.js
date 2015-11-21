6050cc58273f048a3336697c1755d3efcd4d927c
Kaysser Emad Kayyali <kaysserk.admin@LMRC6494.hsd1.state.comcast.net>
2015-11-20 21:53:36 -0500

    Add in the delete button for when doing a new payroll entry and making mistake on an item

--- app/assets/javascripts/payrolls.js
+++ app/assets/javascripts/payrolls.js
@@ -30,20 +30,32 @@ var handle_pay_input_submission = function() {
     	$('#pay_input').val('')
     	$('#employee_name').focus()
     if (name != undefined && gross_pay != "" && gross_pay > 0) {
-    	entries_count = entries_count + 1
 	    var  medicare_tax = (gross_pay * .0145),
 	    	employers_tax = (gross_pay * .062),
 	    	net_pay = roundToTwo((gross_pay - medicare_tax - employers_tax)),
 	    	new_payroll_data = {
-	    		name 	  :  employee_name,
-	    		gross     :  gross_pay,
+	    		id        : entries_count,
+	    		name 	  : employee_name,
+	    		gross     : gross_pay,
 	    		medicare  : roundToTwo((gross_pay * .0145)),
 	    		employers : roundToTwo((gross_pay * .062)),
-	    		net 	  :  net_pay
+	    		net 	  : net_pay
 	    	},
 	    	new_item = "<li class='payroll_item clearfix' id ='"+(entries_count)+"'> <div class='col-xs-3 payroll_item_" + entries_count + "_name'><b>" + employee_name + "</b></div><div class='col-xs-9 text-right payroll_item_" + entries_count + "_info'><span class='gross_pay'>Gross Pay: $" +gross_pay+ "</span> Employers Tax: <b>$" + roundToTwo(employers_tax) + "</b> Medicare Tax: <b>$" + roundToTwo(medicare_tax) + "</b> Net Pay: <b>$" + net_pay + "</b></div></li>";
 		$('#payroll_items').prepend(new_item);
+		$('#'+entries_count+'').prepend("<button class='btn btn-danger btn-xs delete_payroll_item'>x</button")
+		$('#'+entries_count+'').on('click', function(event) {
+			var id = $(event.currentTarget).attr('id');
+			payload.payroll_data.forEach(function(item){
+				if (item.id == id) {
+					payload.payroll_data.splice(payload.payroll_data.indexOf(item), 1);
+				} 
+			});
+			$(event.currentTarget).remove();
+			console.log(payload)
+		})
 		payload.payroll_data.push(new_payroll_data);
+		entries_count = entries_count + 1
 	} 
 	else if (name == undefined || name == "" ) {
 		alertBox("Name is empty or invalid");
@@ -72,7 +84,7 @@ var handle_payroll_save = function() {
 var handle_payroll_post_success = function() {
 	successBox("Payroll entry saved. Redirecting...", {
 		redirect_address : "/admin/payrolls",
-		timer 			 : 1500
+		timer 			 : 900
 	});
 }
 var handle_payroll_post_failure = function() {
