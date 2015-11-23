35307be2a6c4ef69d85e51284d870750ed6a6a99
Kaysser Emad Kayyali <kaysserk.admin@LMRC6494.local>
2015-11-23 18:58:28 -0500

    Add employee model and controller. Need to setup resources and add links to home controller to show new capabilities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121011723) do
+ActiveRecord::Schema.define(version: 20151123235150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 20151121011723) do
     t.boolean  "read",       default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "number"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "invoices", force: :cascade do |t|
