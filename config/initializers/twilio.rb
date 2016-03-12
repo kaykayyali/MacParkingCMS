b7876c65371eef23fdcf2778d96bff80e33b6db4
Kaysser Kayyali <kaykayyali@gmail.com>
2016-03-12 17:04:05 -0500

    remove .env from git

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,5 +1,1 @@
-account_sid = ENV["TWILIO_APP_ID"]
-auth_token = ENV["TWILIO_APP_AUTH_TOKEN"]
-
-
-Twilio_Client = Twilio::REST::Client.new account_sid, auth_token
+Twilio_Client = Twilio::REST::Client.new ENV["TWILIO_APP_ID"], ENV["TWILIO_APP_AUTH_TOKEN"]
